bb08160b060bf52927312fb0166c101d86668fe7
Muhamadjoni Rahimzod <mrahimzod@gmail.com>
2021-11-09 14:18:51 +0400

    changed authentications | bathroom controller - index - geocoding and maps

--- app/models/bathroom.rb
+++ app/models/bathroom.rb
@@ -6,4 +6,6 @@ class Bathroom < ApplicationRecord
   validates :address, presence: true
   validates :price, presence: true, numericality: { only_integer: true}
   has_one_attached :photo
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
