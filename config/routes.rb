b6c3da0f56ff15861a5526390ad550e33a647600
Muhamadjoni Rahimzod <mrahimzod@gmail.com>
2022-03-06 02:13:22 +0400

    deleted nec. routes resources

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,6 @@ Rails.application.routes.draw do
       get :my_bookings
     end
   end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
