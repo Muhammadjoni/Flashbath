c21b065306628c1eee32382b9f57ee46831ce569
Muhamadjoni Rahimzod <mrahimzod@gmail.com>
2022-03-14 15:19:04 +0400

    ideas config

---
+++ config/Ideas.py
@@ -0,0 +1,8 @@
+Strangers app \
+
+An app that gives stangers an opportunity to share the moment together...  // Tinder - Instagram - Twitter collided...
+
+Top info \ 
+
+Don't waste your precious time on social media, get the top info you need and leave... Don't thank us. Our mission is to save you time and keep you productive. // Instagram - Facebook(now Meta) - Twitter
+
